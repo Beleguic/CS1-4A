af07e242ed22157fc7a22c9fc12f9ba123c30026
Jp Hayek <jphayek1@gmail.com>
2023-12-15 22:25:15 +0100

    Devis Entity Construction

--- src/Entity/Devis.php
+++ src/Entity/Devis.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\DevisRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DevisRepository::class)]
@@ -13,8 +14,53 @@ class Devis
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $clientName = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $totalPrice = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2)]
+    private ?string $taxes = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getClientName(): ?string
+    {
+        return $this->clientName;
+    }
+
+    public function setClientName(string $clientName): static
+    {
+        $this->clientName = $clientName;
+
+        return $this;
+    }
+
+    public function getTotalPrice(): ?string
+    {
+        return $this->totalPrice;
+    }
+
+    public function setTotalPrice(string $totalPrice): static
+    {
+        $this->totalPrice = $totalPrice;
+
+        return $this;
+    }
+
+    public function getTaxes(): ?string
+    {
+        return $this->taxes;
+    }
+
+    public function setTaxes(string $taxes): static
+    {
+        $this->taxes = $taxes;
+
+        return $this;
     }
 }
