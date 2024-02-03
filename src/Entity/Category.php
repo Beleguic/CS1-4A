891facabcdc4b1f508bb8acd33dd75a13a80a567
Jp Hayek <jphayek1@gmail.com>
2024-02-03 19:40:40 +0100

    Fix-Category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -30,6 +30,13 @@ class Category
         $this->products = new ArrayCollection();
     }
 
+
+    public function __toString()
+    {
+        return $this->getName();
+    }
+
+
     public function getId(): ?int
     {
         return $this->id;
