9615dd787672186992e2a76f568df7ef7471312b
Thibault Beleguic <thibault.beleguic2@gmail.com>
2024-02-27 00:13:17 +0100

    add many product interface

--- src/Form/FactureType.php
+++ src/Form/FactureType.php
@@ -12,8 +12,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
 class FactureType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
