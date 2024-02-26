f46d5e4126754c6a865d6b9a07d8323e083d7027
Thibault Beleguic <thibault.beleguic2@gmail.com>
2024-02-26 23:13:58 +0100

    readd vich

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 
 ];
