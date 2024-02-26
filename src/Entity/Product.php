ddb989e23bb547063f7234cd98e177f86fa65044
Thibault Beleguic <thibault.beleguic2@gmail.com>
2024-02-26 20:44:51 +0100

    add many product interface

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -17,20 +17,32 @@ class Product
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
-    private ?string $price = null;
-
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
-    #[ORM\ManyToOne(inversedBy: 'products')]
+    #[ORM\Column]
+    private ?int $price = null;
+
+    #[ORM\Column]
+    private ?float $tva = null;
+
+    #[ORM\ManyToOne(inversedBy: 'category')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Category $category = null;
 
-    public function getId(): ?int
+
+    public function getId(): ?string
     {
         return $this->id;
     }
+
+    public function setId(string $id): static
+    {
+        $this->name = $id;
+
+        return $this;
+    }
+
 
     public function getName(): ?string
     {
@@ -44,39 +56,53 @@ class Product
         return $this;
     }
 
-    public function getPrice(): ?string
-    {
-        return $this->price;
-    }
-
-    public function setPrice(string $price): static
-    {
-        $this->price = $price;
-
-        return $this;
-    }
-
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
-    public function setDescription(?string $description): static
+    public function setDescription(string $description): static
     {
         $this->description = $description;
 
         return $this;
     }
 
-    public function getCategory(): ?category
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getTva(): ?float
+    {
+        return $this->tva;
+    }
+
+    public function setTva(float $tva): static
+    {
+        $this->tva = $tva;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
     {
         return $this->category;
     }
 
-    public function setCategory(?category $category): static
+    public function setCategory(?Category $category): static
     {
         $this->category = $category;
 
         return $this;
     }
+
+
 }
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Category;
+use App\Entity\Client;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,12 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du produit',
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Petite description du Produit',
+            ])
         ;
     }
 
