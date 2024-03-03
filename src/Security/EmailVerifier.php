4b1740e31631f2d6975b0663571b5f6692238dd1
Thibault Beleguic <thibault.beleguic2@gmail.com>
2024-03-04 00:34:13 +0100

    hot Fix

--- src/Security/EmailVerifier.php
+++ src/Security/EmailVerifier.php
@@ -8,13 +8,13 @@ use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-//use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
-//use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
 class EmailVerifier
 {
     public function __construct(
-       // private VerifyEmailHelperInterface $verifyEmailHelper,
+        private VerifyEmailHelperInterface $verifyEmailHelper,
         private MailerInterface $mailer,
         private EntityManagerInterface $entityManager
     ) {
