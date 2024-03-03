4e4bb3b4678029c4db22b6f6d810e0d9910a73da
clement-Yvars <clement.yvars2001@gmail.com>
2024-03-03 12:53:03 +0100

    fin mail

--- migrations/Version20240303113015.php
+++ migrations/Version20240303113015.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240301210705 extends AbstractMigration
+final class Version20240303113015 extends AbstractMigration
 {
     public function getDescription(): string
     {
